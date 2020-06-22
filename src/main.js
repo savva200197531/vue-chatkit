5fa37bb677c7ddfdfb70172cd4494308c62998fe
savva200197531 <kashin.savva@mail.ru>
2020-06-22 22:38:22 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
-import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueChatScroll from 'vue-chat-scroll';
 
-Vue.config.productionTip = false
+import App from './App.vue'
+import router from './router'
+import store from './store/index'
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+Vue.use(VueChatScroll);
 
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
